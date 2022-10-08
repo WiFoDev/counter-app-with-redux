107bf41eb733c8e29b7e72a45c919ef2b0424d72
WiFoDev <lou81804@gmail.com>
2022-10-07 23:22:27 -0500

    add card ui component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
+import { Card } from './components/UI/Card'
 
 export const App = () => {
   return (
     <main>
       <section>
-        <div className="card">
+        <Card className="card">
           <h1 className="title">redux counter</h1>
           <h1 className="counter">-- counter value --</h1>
           <button className="toggle__button">Toggle Counter</button>
-        </div>
+        </Card>
       </section>
     </main>
   )
---
+++ src/components/UI/Card/index.jsx
@@ -0,0 +1,5 @@
+import { card } from './styles.module.css'
+
+export const Card = ({ children }) => {
+  return <div className={card}>{children}</div>
+}
