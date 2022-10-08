04768ef984e10dfa2214e39dfd35ea4b81ca20a4
WiFoDev <lou81804@gmail.com>
2022-10-08 01:10:13 -0500

    implement increment and decrement handlers

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,16 +1,32 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { Card } from '../UI/Card'
 
-import { title, counter, toggle__button } from './styles.module.css'
+import { title, counter, button, button__container } from './styles.module.css'
 export const Counter = () => {
   const counterState = useSelector((state) => state.counter)
+  const counterDispatcher = useDispatch()
+
+  const incrementHandler = () => {
+    counterDispatcher({ type: 'increment' })
+  }
+  const decrementHandler = () => {
+    counterDispatcher({ type: 'decrement' })
+  }
 
   return (
     <Card>
       <h1 className={title}>redux counter</h1>
       <h1 className={counter}>{counterState}</h1>
-      <button className={toggle__button}>Toggle Counter</button>
+      <div className={button__container}>
+        <button className={button} onClick={incrementHandler}>
+          Increment
+        </button>
+        <button className={button} onClick={decrementHandler}>
+          Decrement
+        </button>
+      </div>
+      <button className={button}>Toggle Counter</button>
     </Card>
   )
 }
