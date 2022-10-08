4b4ace1d5926047412b8b1485ccce3e498ed846c
WiFoDev <lou81804@gmail.com>
2022-10-08 00:57:01 -0500

    add counter state

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,11 +1,15 @@
+import { useSelector } from 'react-redux'
+
 import { Card } from '../UI/Card'
 
 import { title, counter, toggle__button } from './styles.module.css'
 export const Counter = () => {
+  const counterState = useSelector((state) => state.counter)
+
   return (
     <Card>
       <h1 className={title}>redux counter</h1>
-      <h1 className={counter}>-- counter value --</h1>
+      <h1 className={counter}>{counterState}</h1>
       <button className={toggle__button}>Toggle Counter</button>
     </Card>
   )
