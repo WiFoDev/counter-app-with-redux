9e3fd94c58b96e0d8a2f845aae38b6336891b8d0
WiFoDev <lou81804@gmail.com>
2022-10-07 23:18:25 -0500

    implement app user interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import './App.css'
 
 export const App = () => {
-  return <h1>Hello World</h1>
+  return (
+    <main>
+      <section>
+        <div className="card">
+          <h1 className="title">redux counter</h1>
+          <h1 className="counter">-- counter value --</h1>
+          <button className="toggle__button">Toggle Counter</button>
+        </div>
+      </section>
+    </main>
+  )
 }
